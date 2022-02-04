1b74929901c4498cca5f51983724150e3bcb87da
Alexander Pikurin <pikchas95@gmail.com>
2022-02-04 16:37:50 +0200

    update7

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,11 @@
 const inputControl = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 
-inputControl.addEventListener('input', handleInputRange);
+inputControl.addEventListener('input', () => {
+    text.style.fontSize = inputControl.value + 'px';
+});
 
-text.style.fontSize = inputControl.value + 'px';
+// text.style.fontSize = inputControl.value + 'px';
 
 // function handleInputRange (event) {
 // //   text.style.fontSize = event.currentTarget.value + 'px';  
