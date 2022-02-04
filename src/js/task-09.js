b162a3aa1ca61fe23194d8a2fa4550c8b922a46b
Alexander Pikurin <pikchas95@gmail.com>
2022-02-04 09:17:21 +0200

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,21 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const widgetEl = {
+  body: document.body,
+  colorName: document.querySelector('.color'),
+  buttonChange: document.querySelector('.change-color'),
 }
+
+widgetEl.buttonChange.addEventListener('click', changeColor)
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  const color = getRandomHexColor();
+  widgetEl.colorName.textContent = color;
+  document.body.style.backgroundColor = color;
+      
+};
